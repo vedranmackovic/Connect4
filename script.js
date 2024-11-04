639ecab14b8039f8de7aae1ddd5c957254bcc38f
vedranmackovic <vekijela@gmail.com>
2024-11-04 19:14:12 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,65 @@
+//Row changing color on hover
+function changeColorf1(size, color){
+    for(var i = 0; i< f1.length; i++){
+        f1[i].style.border = size +"px dashed "+ color;
+    }
+}var f1 = $(".f1");
+for(var i = 0; i < f1.length; i++){
+    f1[i].onmouseover = function(){changeColorf1("3","green");}
+    f1[i].onmouseout = function(){changeColorf1("2","brown");}
+}
+function changeColorf2(size, color){
+    for(var i = 0; i< f2.length; i++){
+        f2[i].style.border = size +"px dashed "+ color;
+    }
+}var f2 = $(".f2");
+for(var i = 0; i < f2.length; i++){
+    f2[i].onmouseover = function(){changeColorf2("3","green");}
+    f2[i].onmouseout = function(){changeColorf2("2","brown");}
+}
+function changeColorf3(size, color){
+    for(var i = 0; i< f3.length; i++){
+        f3[i].style.border = size +"px dashed "+ color;
+    }
+}var f3 = $(".f3");
+for(var i = 0; i < f3.length; i++){
+    f3[i].onmouseover = function(){changeColorf3("3","green");}
+    f3[i].onmouseout = function(){changeColorf3("2","brown");}
+}
+function changeColorf4(size, color){
+    for(var i = 0; i< f4.length; i++){
+        f4[i].style.border = size +"px dashed "+ color;
+    }
+}var f4 = $(".f4");
+for(var i = 0; i < f1.length; i++){
+    f4[i].onmouseover = function(){changeColorf4("3","green");}
+    f4[i].onmouseout = function(){changeColorf4("2","brown");}
+}
+function changeColorf5(size, color){
+    for(var i = 0; i< f5.length; i++){
+        f5[i].style.border = size +"px dashed "+ color;
+    }
+}var f5 = $(".f5");
+for(var i = 0; i < f5.length; i++){
+    f5[i].onmouseover = function(){changeColorf5("3","green");}
+    f5[i].onmouseout = function(){changeColorf5("2","brown");}
+}
+function changeColorf6(size, color){
+    for(var i = 0; i< f6.length; i++){
+        f6[i].style.border = size +"px dashed "+ color;
+    }
+}var f6 = $(".f6");
+for(var i = 0; i < f6.length; i++){
+    f6[i].onmouseover = function(){changeColorf6("3","green");}
+    f6[i].onmouseout = function(){changeColorf6("2","brown");}
+}
+function changeColorf7(size, color){
+    for(var i = 0; i< f7.length; i++){
+        f7[i].style.border = size +"px dashed "+ color;
+    }
+}var f7 = $(".f7");
+for(var i = 0; i < f7.length; i++){
+    f7[i].onmouseover = function(){changeColorf7("3","green");}
+    f7[i].onmouseout = function(){changeColorf7("2","brown");}
+}
+
